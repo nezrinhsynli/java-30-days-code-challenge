3b9485d9922fb45fced744f8922a205e90bb27e0
nezrinhsynli <nezrin.huseyinli06@gmail.com>
2024-12-25 23:56:53 +0400

    The first day's task is completed.

---
+++ src/Day1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        //Hello, World
+        System.out.println("Hello, World.");
+        System.out.println(input);
+    }
+
+}
