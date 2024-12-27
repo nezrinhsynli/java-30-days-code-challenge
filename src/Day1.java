2acac31331385d3715db64f688e0f83dd87ad4c2
nezrinhsynli <nezrin.huseyinli06@gmail.com>
2024-12-27 17:46:59 +0400

    Day 2 task completed

--- src/Day1.java
+++ src/Day1.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Day1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println(i+4);
+        System.out.println(d+4);
+        System.out.println("HackerRank " + s);
+    }
 }
