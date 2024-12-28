11e64d6411478cce1f23a213f09e51d08fc92010
nezrinhsynli <nezrin.huseyinli06@gmail.com>
2024-12-28 22:16:22 +0400

    The third day's task is completed

--- src/Day3/Solution.java
+++ src/Day3/Solution.java
@@ -1,4 +1,24 @@
 package Day3;
 
+import java.util.Scanner;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+
+        if (N % 2 != 0 || N == 20) {
+            System.out.println("Weird");
+        } else if (N < 6) {
+            System.out.println("Not Weird");
+        } else if (N < 20) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+
+
+    }
 }
