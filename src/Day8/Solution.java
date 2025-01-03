fc861d4342c01efae56871557b58c1c0a48523f6
nezrinhsynli <nezrin.huseyinli06@gmail.com>
2025-01-04 02:46:31 +0400

    The 8th day's task is completed

---
+++ src/Day8/Solution.java
@@ -0,0 +1,29 @@
+package Day8;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        HashMap<String, Integer> phoneBook = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name, phone);
+        }
+
+        while (in.hasNext()) {
+            String s = in.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
